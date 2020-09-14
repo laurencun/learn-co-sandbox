17abd954f8205c60711a2268c11f0e5a5128dacc
Lauren Cunningham <lauren.busbee@gmail.com>
2020-09-14 01:48:27 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,23 @@
+counter = 0 
+pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
+
+while pets[counter]
+do 
+  puts pets[counter]
+  counter += 1 
+end
+
+def output_array_elements (array)
+  counter = 0 
+  
+  while
+array[counter] do 
+  puts array[counter]
+end
+end
+
+output_array_elements
+
+
+array = ["Spring", "Summer", "Fall", "Winter"]
+array.length 
---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+current_weather = "raining"
+
+case current_weather
+ when "sunny"
+   puts "grab some sunscreen"
+  when "raining"
+    puts "grab an embrella"
+  when "snowing"
+    puts "bundle up"
+end
